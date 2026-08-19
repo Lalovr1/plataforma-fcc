cd18da1e9b9146e017d34379161cdd7636e13f52
Lalovr1 <vargasramirezlalo@gmail.com>
2026-08-18 18:41:35 -0600

    corrige redireccion raiz en Next 16

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   const supabase = createServerComponentClient({ cookies: () => cookieStore });
 
   const {
